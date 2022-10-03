aff94df665467cdce31c5665bb57c4b2b14ca905
VikaAkhapkina <akhapkina_viktoriya@mail.ru>
2022-10-03 21:03:36 +0300

    Added Second View Controller

--- Navigation/Navigation/ViewController.swift
+++ Navigation/Navigation/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func OpenButtenPressed() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let destination = storyboard.instantiateViewController(withIdentifier: "SecondViewController") // будет храниться экран
+        guard let secontViewController = destination as? SecondViewController else {
+            return
+        }//приведение типа
+        secontViewController.text = textField.text //чтобы положить текст из textField
+        
+        present(destination, animated: true) // модальное представление
+    }
+    
 }
 
